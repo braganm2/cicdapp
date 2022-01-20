0d1a61d9f156061f7b124efbf3fe4116b1b7618e
braganm2 <75120705+braganm2@users.noreply.github.com>
2022-01-20 18:20:34 +0000

    commit after merge

--- test.js
+++ test.js
@@ -1,4 +1,9 @@
-var assert = require('assert');
+const assert = require('assert');
+const app = require('./src/app.js');
+const should = require('chai').should();
+const request = require('supertest');
+
+const { add, sub } = require('../src/index');
 
 describe('Dummy test cases', function () {
   describe('Test Case A', function () {
@@ -17,4 +22,20 @@ describe('Dummy test cases', function () {
     // });
   });
 
-});
+  // describe('Main route', function () {
+  //   const version = process.env.VERSION || 'local'
+  //   it('should return Hello World!', function (done) {
+  //     request(app)
+  //       .get('/')
+  //       .expect(200)
+  //       .expect(function (res) {
+  //         res.text.should.equal(`Hello World! \n Version: ${version}`);
+  //       })
+  //       .end(function (err, res) {
+  //         if (err) throw err;
+  //         done();
+  //       });
+  //   });
+  // });
+
+//});
